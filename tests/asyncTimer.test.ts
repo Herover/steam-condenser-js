96254177749f8afbd43947d5791526106f9a616c
Herover <leon.l.a.nielsen@gmail.com>
2020-06-09 15:48:17 +0200

    Add typescript support to tests with ts-jest

--- tests/asyncTimer.test.ts
+++ tests/asyncTimer.test.ts
@@ -1,10 +1,9 @@
-/* eslint-disable */
-const asyncTimer = require('../dist/SteamCondenser/asyncTimer.js');
+import * as asyncTimer from '../lib/SteamCondenser/asyncTimer';
 
 test('resolving doWithin should return resolved value', async () => {
   const expectedResult = 1;
 
-  const val = await asyncTimer.doWithin(new Promise(resolve => resolve(expectedResult)), 1);
+  const val = await asyncTimer.doWithin(new Promise((resolve) => resolve(expectedResult)), 1);
 
   expect(val).toBe(expectedResult);
 });
@@ -14,12 +13,14 @@ test('rejecting doWithin should throw error', async () => {
 
   let err;
   try {
-    const val = await asyncTimer.doWithin(new Promise((resolve, reject) => reject(new Error(rejectMessage))), 1)
-  }
-  catch (e) {
+    await asyncTimer.doWithin(
+      new Promise((resolve, reject) => reject(new Error(rejectMessage))),
+      1,
+      );
+  } catch (e) {
     err = e;
   }
-  
+
   expect(err).not.toBeUndefined();
   expect(err.message).toBe(rejectMessage);
 });
@@ -27,12 +28,11 @@ test('rejecting doWithin should throw error', async () => {
 test('not resolving or rejecting promise should throw error', async () => {
   let err;
   try {
-    const val = await asyncTimer.doWithin(new Promise((resolve, reject) => () => {}), 1)
-  }
-  catch (e) {
+    await asyncTimer.doWithin(new Promise(() => () => {}), 1);
+  } catch (e) {
     err = e;
   }
-  
+
   expect(err).not.toBeUndefined();
   expect(err.message).toBe('Timed out.');
 });
