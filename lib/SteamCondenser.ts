8f3fd61b70594b7b18cfd335abe9af86254c33f8
Herover <leon.l.a.nielsen@gmail.com>
2020-06-19 21:11:03 +0200

    Add GoldSrcServer and socket
    No RCON support, broken rules and players packet handling

--- lib/SteamCondenser.ts
+++ lib/SteamCondenser.ts
@@ -1,3 +1,4 @@
 export * from './SteamCondenser/Servers/MasterServer';
 export * from './SteamCondenser/Servers/SourceServer';
+export * from './SteamCondenser/Servers/GoldSrcServer';
 export * from './SteamCondenser/Servers/SteamPlayer';
